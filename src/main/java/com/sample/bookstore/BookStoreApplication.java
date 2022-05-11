b873a02311d9a08a401e9dc55cec60eb63c51a8a
Shilpita Nag <shilpita.nag@gmail.com>
2022-05-11 14:45:12 +0530

    Model and Repo Definition along with db configuration

--- src/main/java/com/sample/bookstore/BookStoreApplication.java
+++ src/main/java/com/sample/bookstore/BookStoreApplication.java
@@ -2,7 +2,11 @@ package com.sample.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.baeldung.persistence.repo")
+@EntityScan("com.sample.bookstore.db.model")
 @SpringBootApplication
 public class BookStoreApplication {
 
